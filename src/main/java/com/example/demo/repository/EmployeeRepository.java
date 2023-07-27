ce0dfbaabec71d95fc86eb6134e40249893ae97c
KacperFilipiuk <kacper.filipiuk@savangard.com>
2023-07-27 16:00:35 +0200

    Template Controllers

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,6 +1,9 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
+@Repository
 public class EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
